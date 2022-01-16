e52e33f0033c815e27b9816bb0c2b907867c83b9
unknown <hasaninft1992@gmail.com>
2022-01-16 09:58:30 +0330

    merg

--- yadesh/src/Components/AboutYadesh.js
+++ yadesh/src/Components/AboutYadesh.js
@@ -2,22 +2,22 @@ import AboutYadesh from "../assets/Img/AboutYadesh/Rectangle 857.jpg"
 const Aboutyadesh = () => {
     return (
         <div className="w-[100%] h-[100%] bg-[#000] flex pb-[7rem]">
-            <section className="w-[50%] h-[100%]  flex justify-center ">
-                <img src={AboutYadesh} className="w-[70%] h-[50%] rounded mr-[7rem] " />ّ
+            <section className="w-[606.86px] h-[360px]  flex justify-center mr-[18px] ">
+                <img src={AboutYadesh} className="w-[606.86px] h-[360px] rounded mr-[7rem] " />ّ
             </section>
-            <section className="w-[50%] opacity-80 flex flex-col justify-center h-[50vh] ">
-                <div className="mr-[5.5rem]">
-                    <div className="flex text-[#E6FFF3] ">
-                        <div className="border-[#00FF85] opacity-80  border-r-[4px] ml-[7px] rounded" > </div>
-                        <h6 className="">درباره یادش</h6>
+            <section className="w-[590px] h-[360px] flex flex-col justify-center  mr-[30px]  ">
+                <div className="mr-[85px]">
+                    <div className="flex text-[#E6FFF3] w-[178px] h-[22px] text-[22px]">
+                        <div className="border-[#00FF85]  border-r-[7px] ml-[7px] rounded h-[22px] mt-[8px]" > </div>
+                        <h6 className="font-medium">درباره یادش</h6>
                     </div>
-                    <h6 className="text-[#E6FFF3] mt-[1rem] text-[13px]"> درباره ما بیشتر بدانید</h6>
+                    <h6 className="text-[#C4C4C4] text-[15px] w-[189px] h-[18px] mt-[23px]"> درباره ما بیشتر بدانید</h6>
                 </div>
-                <div className="text-[#E6FFF3] text-[13px] mt-[1rem] leading-8 flex justify-center">
-                    <p className="w-[500px] text-justify">یادگیری دغدغه همیشگی افراد است ولی بستر مناسبی برای آن وجود ندارد. یادش یک پلتفرم آموزش برای شرکت هااساتید مدیران تولید محتواواراد است که دسترسی دسرسی به ویدیوهای آموزشی را برای کاربران آسان کرده است.</p>
+                <div className="text-[#C4C4C4] text-[13px] pt-[30px] leading-[40px] flex justify-center mr-[80px]">
+                    <p className="w-[521px] h-[120px] text-justify">یادگیری دغدغه همیشگی افراد است ولی بستر مناسبی برای آن وجود ندارد. یادش یک پلتفرم آموزش برای شرکت هااساتید مدیران تولید محتواواراد است که دسترسی دسرسی به ویدیوهای آموزشی را برای کاربران آسان کرده است.</p>
                 </div>
-                <div className="mt-[1rem] mr-[5rem]">
-                    <button className="text-[12px] text-[#E6FFF3] w-[7.5rem] h-[2.5rem] rounded border-solid border-[#00FF85] border-2 ml-[7px] "> بیشتر بدانید</button>
+                <div className="mt-[30px] mr-[83px]">
+                    <button className=" font-extralight text-[12px] text-[#E6FFF3] w-[160px] h-[46px] rounded border-solid border-[#00FF85] border-2 ml-[7px] "> بیشتر بدانید</button>
                 </div>
             </section>
         </div>
--- yadesh/src/Components/MostView.js
+++ yadesh/src/Components/MostView.js
@@ -11,13 +11,13 @@ import bgLogoPlay from "../assets/Img/MostVeiw/bgLogoPlay.png"
 const MostView = () => {
     return (
         <div className="w-[100%] h-[100vh] bg-[#000]">
-            <section className=" w-[80%] flex flex-col items-center ">
-                <div className="flex w-[80%]  justify-start">
-                    <div className="border-[#00FF85] opacity-80  border-r-[4px] ml-[6px] rounded " > .</div>
-                    <h6 className="text-[#E6FFF3] text-[24px]">پربازدیدترین اپیزودها</h6>
+            <section className=" w-[266px] flex flex-col items-center">
+                <div className="flex w-[266px]  justify-start mr-[20px]">
+                    <div className="border-[#00FF85] opacity-80  border-r-[7px] ml-[6px] rounded h-[20px] mt-[8px] " > </div>
+                    <h6 className="text-[#E6FFF3] text-[22px] w-[266px] h-[29px]">پربازدیدترین اپیزودها</h6>
                 </div>
                 <div className="flex w-[80%]  justify-start">
-                    <h6 className="text-[#E6FFF3] mt-[1rem] text-[18px] opacity-80">اپیزودها پربازدید هفته</h6>
+                    <h6 className="text-[#C4C4C4] mt-[1rem] text-[16px] w-[174px] h-[17px] font-extralight">اپیزودها پربازدید هفته</h6>
                 </div>
             </section>
             <section className="w-[100%] h-[557px] flex mt-[2rem] justify-center">
@@ -25,14 +25,14 @@ const MostView = () => {
                     <img src={imgPlayVideo} className="rounded w-[459.99px] h-[275px] blur-[2px] bg-current" />
                     <img src={LogoPlay} className="absolute top-[95px] right-[315px] blur-[none] z-10 w-[30px] " />
                     <img src={bgLogoPlay} className="absolute  top-[78px] right-[300px] w-[65px] " />
-                    <div className="text-[12px] text-[#E6FFF3]  mt-[2rem] flex flex-col items-start w-[80%] leading-8">
-                        <h6 className="text-[18px]"> مقدمات حسابداری کلان -درامد وهزینه</h6>
-                        <h6 className="text-[14px]"> در این درس به ساختار هزینه و جریان های درامد می پردازیم</h6>
-                        <h6 className="text-[12px]"> 45 دقیقه و 26 ثانیه . 3652 بازدید</h6>
+                    <div className="text-[12px] text-[#E6FFF3]  mt-[20px] flex flex-col items-start ">
+                        <h6 className="text-[18px] w-[319px] h-[40px]"> مقدمات حسابداری کلان -درامد وهزینه</h6>
+                        <h6 className="text-[13px] w-[460px] h-[78px] text-[#C4C4C4] leading-[30px]">   در این درس به ساختار هزینه و جریان های درامد می پردازیم و مباحث حسابداری کلان رو مورد بررسی قرار می دهیم</h6>
+                        <h6 className="text-[12px] w-[200px] h-[20px] text-[#C4C4C4] mt-[5px]"> 45 دقیقه و 26 ثانیه . 3652 بازدید</h6>
                     </div>
                 </div>
-                <div className="flex flex-wrap w-[750px] ">
-                    <div className="text-[#E6FFF3] text-[11px] mb-[1rem]">
+                <div className="flex flex-wrap w-[750px] h-[460px]">
+                    <div className="text-[#E6FFF3] text-[11px] mb-[15px] ml-[10px] ">
                         <img src={CultureBuilding} className="rounded w-[216px] h-[135.52px]" />
                         <div className="leading-7 mt-[.5rem] opacity-75">
                             <h6 className="text-[14px]"> فرهنگ سازمانی - مدیریت محیط</h6>
