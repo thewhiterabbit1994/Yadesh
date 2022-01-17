81cdaa9b13b2a63ebf7f3696267ea7282d3ac378
Ho3ein Zarrabi <ho3einzarrabi@gmail.com>
2022-01-17 17:17:03 +0330

    fix test

---
+++ yadesh/src/Components/UserProfile/EditProfileModal.js
@@ -0,0 +1,87 @@
+import CloseButtonEditProfileModal from "../../assets/svg/CloseButtonEditProfileModal";
+import UserProfileImg from "../../assets/Img/UserProfileImg.png";
+import UpArrow from "../../assets/svg/UpArrow";
+
+const EditProfileModal = ({ editProfileModal, seteditProfileModal }) => {
+  return (
+    <div
+      className={`absolute z-30 w-full h-[2830px] bg-[#00000071]  ${
+        editProfileModal ? null : "hidden"
+      }`}
+    >
+      <div className="w-[450px] h-[530px] fixed right-[30%] mt-[20px] rounded-[10px] bg-[#fff]">
+        <div
+          onClick={() => seteditProfileModal(false)}
+          className="cursor-pointer absolute left-6 top-6"
+        >
+          <CloseButtonEditProfileModal />
+        </div>
+        <div>
+          <p className="text-[15px] font-bold mt-6 mr-6">
+            پروفایل خود را کامل کنید
+          </p>
+          <p className="text-[12px] mt-2 mr-6 ">
+            خودتان را به هم کلاسی هایتان معرفی کنید
+          </p>
+        </div>
+        <div className="flex mt-12 mr-12">
+          <div>
+            <img className="rounded-full h-[120px]" src={UserProfileImg} />
+          </div>
+          <div className="flex flex-col mr-5">
+            <button className="text-white text-[13px] bg-[#002433] flex mr-[15px] mt-[11px] w-[200px] h-[45px] rounded-[5px]">
+              <div className="mt-[13px] mr-[40px]">
+                <UpArrow />
+              </div>
+              <p className="mt-[12px] mr-[15px]">آپلود عکس جدید</p>
+            </button>
+            <button className="text-[#002433] text-[13px] border-[1.5px] border-[#002433] bg-[white] flex mr-[15px] mt-[11px] w-[200px] h-[45px] rounded-[5px]">
+              <p className="mt-[1px] mr-[35px] text-[30px]">×</p>
+              <p className="mt-[12px] mr-[15px]">حذف عکس فعلی </p>
+            </button>
+          </div>
+        </div>
+        <div className="h-[1.5px] w-[355px] m-auto mt-5 bg-[#81818150]"></div>
+        <div className="flex flex-col items-center m-auto w-full">
+          <div className="mt-2">
+            <label htmlFor="name" className="mr-1 text-[12px]">
+              {"نام "}
+            </label>
+            <label htmlFor="lastname" className="mr-[170px]  text-[12px]">
+              {"نام خانوادگی"}
+            </label>
+            <br />
+            <input
+              id="name"
+              className="h-[45px] w-[170px] bg-[#E6E9EB]  rounded-[5px] pr-5 mt-2 text-[12px]"
+              type="text"
+            />
+
+            <input
+              id="lastname"
+              className="h-[45px] w-[170px] bg-[#E6E9EB] mr-[18px] rounded-[5px] pr-5 text-[12px]"
+              type="text"
+            />
+          </div>
+          <div className="mt-3">
+            <label htmlFor="lastname" className=" text-[12px]">
+              بیوگرافی شما
+            </label>
+            <br />
+            <input
+              id="name"
+              className="h-[45px] w-[360px] bg-[#E6E9EB]  rounded-[5px] pr-5 mt-2 text-[12px]"
+              type="text"
+            />
+          </div>
+          <div>
+            <button className="h-[45px] w-[360px] bg-[#80ffc2]  rounded-[5px] mt-6 text-[12px]">
+              ثبت اطلاعات
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default EditProfileModal;
--- yadesh/src/Components/UserProfile/UserInfo.js
+++ yadesh/src/Components/UserProfile/UserInfo.js
@@ -3,56 +3,71 @@ import UserProfileImg from "../../assets/Img/UserProfileImg.png";
 import Hat from "../../assets/svg/Hat";
 import Star from "../../assets/svg/Star";
 import PenIcon from "../../assets/svg/PenIcon";
+import { useState } from "react";
+import EditProfileModal from "./EditProfileModal";
 
-export default () => (
-  <div className="h-[600px] w-full bg-[#000405] ">
-    <div className="absolute z-10 top-[80px] left-[60px]">
-      <button className="bg-[#072b50] flex mr-[15px] mt-[11px] w-[155px] h-[45px] rounded-[4px] ">
-        <p className="my-[12px] mr-[18px] text-[12.5px] text-[#ffffff]">
-          ویرایش پروفایل
-        </p>
-        <div className="mr-[15px] mt-[14px]">
-          <PenIcon />
-        </div>
-      </button>
-    </div>
-    <div className="bg-gradient-to-t from-[#000000] to-[#00040511] h-[450px] w-full absolute"></div>
-    <div className="">
-      <img className="" src={UserProfileBackground} />
-      <div className="flex flex-col items-center m-auto w-full absolute top-[100px]">
-        <img className="h-[80px] w-[80px] rounded-full" src={UserProfileImg} />
-        <p className="text-white text-[25px] mt-[25px] font-medium">
-          مرتضی بهمنش
-        </p>
-        <p className=" text-[#C4C4C4] mt-[15px] text-[12px]">
-          به دنبال اهدافت برو
-        </p>
-        <div className="flex mt-[30px]  ">
-          <div className="flex flex-col items-center m-auto w-full">
-            <p className="text-white text-[25px] font-medium">۲۸</p>
-            <div className=" ">
-              <p className="mt-[15px] text-[#C4C4C4]  text-[12px]">
-                دوره های شرکت شده
-              </p>
-              <div className="w-full flex flex-col items-center mt-[15px]">
-                <Hat />
+export default () => {
+  const [editProfileModal, seteditProfileModal] = useState(false);
+  return (
+    <div className="h-[600px] w-full bg-[#000405] ">
+      <EditProfileModal
+        seteditProfileModal={seteditProfileModal}
+        editProfileModal={editProfileModal}
+      />
+      <div className="absolute z-10 top-[80px] left-[60px]">
+        <button
+          onClick={() => seteditProfileModal(true)}
+          className="bg-[#072b50] flex mr-[15px] mt-[11px] w-[155px] h-[45px] rounded-[4px] "
+        >
+          <p className="my-[12px] mr-[18px] text-[12.5px] text-[#ffffff]">
+            ویرایش پروفایل
+          </p>
+          <div className="mr-[15px] mt-[14px]">
+            <PenIcon />
+          </div>
+        </button>
+      </div>
+      <div className="bg-gradient-to-t from-[#000000] to-[#00040511] h-[450px] w-full absolute"></div>
+      <div className="">
+        <img className="" src={UserProfileBackground} />
+        <div className="flex flex-col items-center m-auto w-full absolute top-[100px]">
+          <img
+            className="h-[80px] w-[80px] rounded-full"
+            src={UserProfileImg}
+          />
+          <p className="text-white text-[25px] mt-[25px] font-medium">
+            مرتضی بهمنش
+          </p>
+          <p className=" text-[#C4C4C4] mt-[15px] text-[12px]">
+            به دنبال اهدافت برو
+          </p>
+          <div className="flex mt-[30px]  ">
+            <div className="flex flex-col items-center m-auto w-full">
+              <p className="text-white text-[25px] font-medium">۲۸</p>
+              <div className=" ">
+                <p className="mt-[15px] text-[#C4C4C4]  text-[12px]">
+                  دوره های شرکت شده
+                </p>
+                <div className="w-full flex flex-col items-center mt-[15px]">
+                  <Hat />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="h-[55px] w-[3.2px] mt-[35px]  mx-[30px] bg-[#c4c4c448]"></div>
-          <div className="flex flex-col items-center m-auto w-full">
-            <p className="text-white text-[25px]">۱۴۵</p>
-            <div className="">
-              <p className="mt-[15px] text-[#C4C4C4] text-[12px]">
-                اپیزودهای مورد علاقه
-              </p>
-              <div className="w-full flex flex-col items-center mt-[15px]">
-                <Star />
+            <div className="h-[55px] w-[3.2px] mt-[35px]  mx-[30px] bg-[#c4c4c448]"></div>
+            <div className="flex flex-col items-center m-auto w-full">
+              <p className="text-white text-[25px]">۱۴۵</p>
+              <div className="">
+                <p className="mt-[15px] text-[#C4C4C4] text-[12px]">
+                  اپیزودهای مورد علاقه
+                </p>
+                <div className="w-full flex flex-col items-center mt-[15px]">
+                  <Star />
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
