280225f9f643c5cd21224bc0bf9712151aa45894
Ho3ein Zarrabi <ho3einzarrabi@gmail.com>
2022-01-22 13:43:29 +0330

    fix test

--- yadesh/src/Pages/AdminDashboard/ChangPassword.js
+++ yadesh/src/Pages/AdminDashboard/ChangPassword.js
@@ -6,62 +6,62 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const ChangPassword = () => {
-    const [passwordShown, setPasswordShown] = useState(false);
-    const togglePassword = () => {
-        setPasswordShown(!passwordShown);
-    };
-    return (
-        <div className="h-[100vh] w-full flex justify-center items-center bg-gradient-to-br from-[#5eca9f] via-[#60aef3] to-[#f3dd86] ">
-            <div className=" bg-white h-[70vh] w-[25vw] rounded-[10px] flex flex-col items-center m-auto">
-                <div className="my-7">
-                    <YadeshLightLogo />
-                </div>
-
-                <p className="mt-1  text-[12px] font-bold text-[#000000]">
-                   رمز رمز عبور جدید را وارد نمایید
-                </p>
-                <label
-                    htmlFor="username"
-                    className="flex text-[#383838] mt-8 mb-3 -mr-[190px] font-bold text-[10px]"
-                >
-                    <div className="ml-2">
-                        <Lock color="#000" />
-                    </div>
-                  رمز عبور جدید
-                </label>
-                <input
-                    id="username"
-                    className="h-[45px] w-[267px] bg-[#f0f2f3] outline-[#000] text-[#000000]   rounded-[5px] pr-5 text-[13px]"
-                    type="text"
-                />
-                <label
-                    htmlFor="password"
-                    className="flex text-[#383838] mt-12 -mr-[170px] font-bold text-[10px]"
-                >
-                    <div className="ml-2">
-                        <Lock color="#000" />
-                    </div>
-                    تکرار رمز عبور جدید
-                </label>
-                <div
-                    onClick={togglePassword}
-                    className=" cursor-pointer relative top-7 mr-[210px]"
-                >
-                    <ShowPass color="#000" />
-                </div>
-                <input
-                    id="password"
-                    className="h-[45px] w-[267px] bg-[#f0f2f3] outline-[#000] text-[#000000]  rounded-[5px] pr-5 text-[13px]"
-                    type={passwordShown ? "text" : "password"}
-                />
-               
-                <Link to="/adminlogin/succsess">
-                    <button className="h-[45px] w-[267px] mt-5 bg-[#008043] text-[#ffffff] font-bold rounded-[5px] text-[11px]">
-                        تغییر رمز عبور
-                    </button>
-                </Link>
-            </div>
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePassword = () => {
+    setPasswordShown(!passwordShown);
+  };
+  return (
+    <div className="h-[100vh] w-full flex justify-center items-center bg-gradient-to-br from-[#5eca9f] via-[#60aef3] to-[#f3dd86] ">
+      <div className=" bg-white h-[430px] w-[25vw] rounded-[10px] flex flex-col items-center m-auto">
+        <div className="my-7">
+          <YadeshLightLogo />
         </div>
-    )
-}
-export default ChangPassword
+
+        <p className="mt-1  text-[12px] font-bold text-[#000000]">
+          رمز رمز عبور جدید را وارد نمایید
+        </p>
+        <label
+          htmlFor="username"
+          className="flex text-[#383838] mt-8 mb-3 -mr-[190px] font-bold text-[10px]"
+        >
+          <div className="ml-2">
+            <Lock color="#000" />
+          </div>
+          رمز عبور جدید
+        </label>
+        <input
+          id="username"
+          className="h-[45px] w-[267px] bg-[#f0f2f3] outline-[#000] text-[#000000]   rounded-[5px] pr-5 text-[13px]"
+          type="text"
+        />
+        <label
+          htmlFor="password"
+          className="flex text-[#383838] mt-12 -mr-[170px] font-bold text-[10px]"
+        >
+          <div className="ml-2">
+            <Lock color="#000" />
+          </div>
+          تکرار رمز عبور جدید
+        </label>
+        <div
+          onClick={togglePassword}
+          className=" cursor-pointer relative top-7 mr-[210px]"
+        >
+          <ShowPass color="#000" />
+        </div>
+        <input
+          id="password"
+          className="h-[45px] w-[267px] bg-[#f0f2f3] outline-[#000] text-[#000000]  rounded-[5px] pr-5 text-[13px]"
+          type={passwordShown ? "text" : "password"}
+        />
+
+        <Link to="/adminlogin/succsess">
+          <button className="h-[45px] w-[267px] mt-5 bg-[#008043] text-[#ffffff] font-bold rounded-[5px] text-[11px]">
+            تغییر رمز عبور
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default ChangPassword;
--- yadesh/src/Pages/AdminDashboard/EnterCode.js
+++ yadesh/src/Pages/AdminDashboard/EnterCode.js
@@ -1,68 +1,142 @@
 import YadeshLightLogo from "../../assets/svg/AdminLogin/YadeshLightLogo";
 import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
 
 const EnterCode = () => {
-    return (
-        <div className="h-[100vh] w-full bg-gradient-to-br from-[#5eca9f] via-[#60aef3] to-[#f3dd86] flex items-center  ">
-            <div className=" bg-white h-[300px] w-[25vw] rounded-[10px] flex flex-col items-center  m-auto">
-                <div className="my-7">
-                    <YadeshLightLogo />
-                </div>
+  const inputE1 = useRef(null);
+  const inputE2 = useRef(null);
+  const inputE3 = useRef(null);
+  const inputE4 = useRef(null);
+  const inputE5 = useRef(null);
+  const inputE6 = useRef(null);
+  const btnSubmit = useRef(null);
+  const [enterCode, setenterCode] = useState("");
 
-                <p className="mt-1  text-[12px] font-bold text-[#000000]">
-                    کد تایید را وارد کنید
-                </p>
-                <label
-                    htmlFor="username"
-                    className="flex text-[#383838] mt-8 mb-3 -mr-[150px] font-bold text-[10px]"
-                >
-                    کد دریافتی را وارد کنید
-                </label>
-                <sextion className="flex  w-[260px] justify-between">
-                    <input
-                        id="username"
-                        className="h-[45px] w-[30px] bg-[#f0f2f3] outline-[#000] text-[#E6FFF3]   rounded-[5px] pr-5 text-[13px]"
-                        type="text"
-                    />
-                    <div className="flex items-center">-</div>
-                    <input
-                        id="username"
-                        className="h-[45px] w-[30px] bg-[#f0f2f3] outline-[#000] text-[#E6FFF3]   rounded-[5px] pr-5 text-[13px]"
-                        type="text"
-                    />
-                    <div className="flex items-center">-</div>
-                    <input
-                        id="username"
-                        className="h-[45px] w-[30px] bg-[#f0f2f3] outline-[#000] text-[#E6FFF3]   rounded-[5px] pr-5 text-[13px]"
-                        type="text"
-                    />
-                    <div className="flex items-center">-</div>
-                    <input
-                        id="username"
-                        className="h-[45px] w-[30px] bg-[#f0f2f3] outline-[#000] text-[#E6FFF3]   rounded-[5px] pr-5 text-[13px]"
-                        type="text"
-                    />
-                    <div className="flex items-center">-</div>
-                    <input
-                        id="username"
-                        className="h-[45px] w-[30px] bg-[#f0f2f3] outline-[#000] text-[#E6FFF3]   rounded-[5px] pr-5 text-[13px]"
-                        type="text"
-                    />
-                    <div className="flex items-center">-</div>
-                    <input
-                        id="username"
-                        className="h-[45px] w-[30px] bg-[#f0f2f3] outline-[#000] text-[#E6FFF3]   rounded-[5px] pr-5 text-[13px]"
-                        type="text"
-                    />
-                </sextion>
-
-                <Link to="/adminlogin/changpassword">
-                    <button className="h-[45px] w-[267px] mt-5 bg-[#008043] text-[#ffffff] font-bold rounded-[5px] text-[11px]">
-                        تایید
-                    </button>
-                </Link>
-            </div>
+  return (
+    <div className="h-[100vh] w-full bg-gradient-to-br from-[#5eca9f] via-[#60aef3] to-[#f3dd86] flex items-center  ">
+      <div className=" bg-white h-[300px] w-[25vw] rounded-[10px] flex flex-col items-center  m-auto">
+        <div className="my-7">
+          <YadeshLightLogo />
         </div>
-    );
-}
-export default EnterCode
+
+        <p className="mt-1  text-[12px] font-bold text-[#000000]">
+          کد تایید را وارد کنید
+        </p>
+        <label
+          htmlFor="username"
+          className="flex text-[#383838] mt-8 mb-3 -mr-[150px] font-bold text-[10px]"
+        >
+          کد دریافتی را وارد کنید
+        </label>
+        <section className="flex flex-row-reverse  w-[260px] justify-between">
+          <input
+            id="username"
+            className="h-[45px] text-center text-[#000] w-[30px] bg-[#f0f2f3] outline-[#000]  rounded-[5px]  text-[13px]"
+            type="number"
+            ref={inputE1}
+            onChange={({ target: { value } }) => {
+              if (!isNaN(Number(value))) {
+                Number(value);
+                setenterCode(enterCode + value);
+                inputE2.current.focus();
+              } else {
+                inputE1.current.value = "";
+              }
+            }}
+          />
+          <div className="flex items-center">-</div>
+          <input
+            id="username"
+            className="h-[45px] text-center text-[#000] w-[30px] bg-[#f0f2f3] outline-[#000]  rounded-[5px]  text-[13px]"
+            type="number"
+            ref={inputE2}
+            onChange={({ target: { value } }) => {
+              if (!isNaN(Number(value))) {
+                Number(value);
+                setenterCode(enterCode + value);
+                inputE3.current.focus();
+              } else {
+                inputE2.current.value = "";
+              }
+            }}
+          />
+          <div className="flex items-center">-</div>
+          <input
+            id="username"
+            className="h-[45px] text-center text-[#000] w-[30px] bg-[#f0f2f3] outline-[#000]  rounded-[5px]  text-[13px]"
+            type="number"
+            ref={inputE3}
+            onChange={({ target: { value } }) => {
+              if (!isNaN(Number(value))) {
+                Number(value);
+                setenterCode(enterCode + value);
+                inputE4.current.focus();
+              } else {
+                inputE3.current.value = "";
+              }
+            }}
+          />
+          <div className="flex items-center">-</div>
+          <input
+            id="username"
+            className="h-[45px] text-center text-[#000] w-[30px] bg-[#f0f2f3] outline-[#000]  rounded-[5px]  text-[13px]"
+            type="number"
+            ref={inputE4}
+            onChange={({ target: { value } }) => {
+              if (!isNaN(Number(value))) {
+                Number(value);
+                setenterCode(enterCode + value);
+                inputE5.current.focus();
+              } else {
+                inputE4.current.value = "";
+              }
+            }}
+          />
+          <div className="flex items-center">-</div>
+          <input
+            id="username"
+            className="h-[45px] text-center text-[#000] w-[30px] bg-[#f0f2f3] outline-[#000]  rounded-[5px]  text-[13px]"
+            type="number"
+            ref={inputE5}
+            onChange={({ target: { value } }) => {
+              if (!isNaN(Number(value))) {
+                Number(value);
+                setenterCode(enterCode + value);
+                inputE6.current.focus();
+              } else {
+                inputE5.current.value = "";
+              }
+            }}
+          />
+          <div className="flex items-center">-</div>
+          <input
+            id="username"
+            className="h-[45px] text-center text-[#000] w-[30px] bg-[#f0f2f3] outline-[#000]  rounded-[5px]  text-[13px]"
+            type="number"
+            ref={inputE6}
+            onChange={({ target: { value } }) => {
+              if (!isNaN(Number(value))) {
+                Number(value);
+                setenterCode(enterCode + value);
+                btnSubmit.current.focus();
+              } else {
+                inputE6.current.value = "";
+              }
+            }}
+          />
+        </section>
+
+        <Link to="/adminlogin/changpassword">
+          <button
+            ref={btnSubmit}
+            className="h-[45px] w-[267px] mt-5 bg-[#008043] text-[#ffffff] font-bold rounded-[5px] text-[11px]"
+          >
+            تایید
+          </button>
+        </Link>
+      </div>
+      {console.log(enterCode)}
+    </div>
+  );
+};
+export default EnterCode;
