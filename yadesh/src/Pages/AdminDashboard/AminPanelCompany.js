28e0f3a7b31c44fd8d5fa313cd13ec2d253066f3
unknown <hasaninft1992@gmail.com>
2022-01-23 09:25:06 +0330

    fixed

--- yadesh/src/Pages/AdminDashboard/AminPanelCompany.js
+++ yadesh/src/Pages/AdminDashboard/AminPanelCompany.js
@@ -1,6 +1,7 @@
 import Header from "../../Components/AdminPanelCourses/Header"
 import MiddlwSection from "../../Components/AdminPanelCourses/MiddlwSection"
 import Table from "../../Components/AdminPanelCourses/Table"
+// import AdminDashboardLayout from "../../Pages/AdminDashboard"
 const AdminPanelCompany = () => {
     return (
         <section className="w-full h-[100vh]">
