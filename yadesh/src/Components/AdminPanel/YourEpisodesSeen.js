366e061e6851598d3be92314c9a33b1b2ebc7e0b
Ho3ein Zarrabi <ho3einzarrabi@gmail.com>
2022-01-30 14:18:57 +0330

    fix test

--- yadesh/src/Components/AdminPanel/YourEpisodesSeen.js
+++ yadesh/src/Components/AdminPanel/YourEpisodesSeen.js
@@ -1,119 +1,48 @@
-import * as am5 from "@amcharts/amcharts5";
-import * as am5xy from "@amcharts/amcharts5/xy";
-import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
-import * as am5radar from "@amcharts/amcharts5/radar";
 import { useEffect, useRef } from "react";
 import SeenIcon from "../../assets/svg/View";
-import { selectOptions } from "@testing-library/user-event/dist/select-options";
+import { ResponsiveBar } from "@nivo/bar";
+import chartbg from "../../assets/Img/chartbg.png";
 
 export default () => {
-  useEffect(() => {
-    let root = am5.Root.new("chart2div");
-    let chart = root.container.children.push(
-      am5xy.XYChart.new(root, {
-        panX: false,
-        panY: false,
-      })
-    );
-
-    root.setThemes([am5themes_Animated.new(root)]);
-    let xRenderer = am5xy.AxisRendererX.new(root, { minGridDistance: 0 });
-    xRenderer.labels.template.setAll({
-      fontSize: "10px",
-      paddingRight: -35,
-    });
-
-    let yRenderer = am5xy.AxisRendererY.new(root, {});
-    yRenderer.labels.template.setAll({
-      fontSize: "0px",
-    });
-
-    let data = [
-      {
-        country: "شنبه",
-        value: 2025,
-      },
-      {
-        country: "یکشنبه",
-        value: 1882,
-      },
-      {
-        country: "دوشنبه",
-        value: 1809,
-      },
-      {
-        country: "سه شنبه",
-        value: 1322,
-      },
-      {
-        country: "چهارشنبه",
-        value: 1122,
-      },
-      {
-        country: "پنجشنبه",
-        value: 1114,
-      },
-      {
-        country: "جمعه",
-        value: 984,
-      },
-    ];
-    // Create Y-axis
-    let yAxis = chart.yAxes.push(
-      am5xy.ValueAxis.new(root, {
-        maxDeviation: 0.3,
-        renderer: yRenderer,
-      })
-    );
-
-    // Create X-Axis
-    let xAxis = chart.xAxes.push(
-      am5xy.CategoryAxis.new(root, {
-        maxDeviation: 0.3,
-        categoryField: "country",
-        renderer: xRenderer,
-        tooltip: am5.Tooltip.new(root, {}),
-      })
-    );
-
-    let series = chart.series.push(
-      am5xy.ColumnSeries.new(root, {
-        name: "Series 1",
-        xAxis: xAxis,
-        yAxis: yAxis,
-        valueYField: "value",
-        sequencedInterpolation: true,
-        categoryXField: "country",
-        tooltip: am5.Tooltip.new(root, {
-          labelText: "{valueY}",
-        }),
-      })
-    );
-
-    series.columns.template.setAll({
-      cornerRadiusTL: 100,
-      cornerRadiusTR: 100,
-    });
-    series.columns.template.adapters.add("fill", (fill, target) => {
-      return chart.get("colors").getIndex(series.columns.indexOf(target));
-    });
-
-    series.columns.template.adapters.add("stroke", (stroke, target) => {
-      return chart.get("colors").getIndex(series.columns.indexOf(target));
-    });
-
-    series.data.setAll(data);
-    xAxis.data.setAll(data);
-
-    series.appear(1000);
-    chart.appear(1000, 100);
-    return () => {
-      root.dispose();
-    };
-  }, []);
-
+  const data = [
+    {
+      weekday: "شنبه",
+      episodeSeen: 70,
+      episodeSeenColor: "#ffc700",
+    },
+    {
+      weekday: "یک شنبه",
+      episodeSeen: 40,
+      episodeSeenColor: "#00ff85",
+    },
+    {
+      weekday: "دوشنبه",
+      episodeSeen: 90,
+      episodeSeenColor: "#0055b3",
+    },
+    {
+      weekday: "سه شنبه",
+      episodeSeen: 20,
+      episodeSeenColor: "#ffc700",
+    },
+    {
+      weekday: "چهارشنبه",
+      episodeSeen: 60,
+      episodeSeenColor: "#00b35d",
+    },
+    {
+      weekday: "پنجشنبه",
+      episodeSeen: 70,
+      episodeSeenColor: "#0172ee",
+    },
+    {
+      weekday: "جمعه",
+      episodeSeen: 30,
+      episodeSeenColor: "#ffc700",
+    },
+  ];
   return (
-    <div className="absolute w-[350px] h-[380px] bg-[#f5f5f5] rounded-[10px] top-16 left-0">
+    <div className="absolute w-[350px] h-[380px] bg-[#f5f5f5] rounded-[10px] top-16 left-4">
       <div>
         <div className="flex mt-5 justify-between">
           <div className="flex mr-5">
@@ -135,7 +64,84 @@ export default () => {
           </div>
         </div>
       </div>
-      <div id="chart2div" className="w-[350px] h-[320px] "></div>
+      <img className="absolute z-10 mt-8 mr-[20px]" src={chartbg} />
+      <div className="w-[500px] mt-[-20px] mr-[-120px] h-[350px]">
+        <ResponsiveBar
+          data={data}
+          keys={["episodeSeen"]}
+          indexBy="weekday"
+          margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+          padding={0.6}
+          valueScale={{ type: "linear" }}
+          indexScale={{ type: "band", round: true }}
+          colors={({ id, data }) => String(data[`${id}Color`])}
+          borderRadius={10}
+          enableGridX={false}
+          enableGridY={false}
+          defs={[
+            {
+              id: "dots",
+              type: "patternDots",
+              background: "inherit",
+              color: "#38bcb2",
+              size: 4,
+              padding: 1,
+              stagger: true,
+            },
+            {
+              id: "lines",
+              type: "patternLines",
+              background: "inherit",
+              color: "#eed312",
+              rotation: -45,
+              lineWidth: 6,
+              spacing: 10,
+            },
+          ]}
+          borderColor={{
+            from: "color",
+            modifiers: [["darker", 1.6]],
+          }}
+          axisTop={null}
+          axisRight={null}
+          axisBottom={{
+            tickSize: 0,
+
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "",
+            legendPosition: "middle",
+            legendOffset: 32,
+          }}
+          axisLeft={null}
+          label={false}
+          legends={[
+            {
+              dataFrom: "keys",
+              anchor: "bottom-right",
+              direction: "column",
+              justify: false,
+              translateX: 120,
+              translateY: 0,
+              itemsSpacing: 0,
+              itemWidth: 0,
+              itemHeight: 0,
+              itemDirection: "left-to-right",
+              itemOpacity: 0,
+              symbolSize: 0,
+              effects: [
+                {
+                  on: "hover",
+                  style: {
+                    itemOpacity: 0,
+                  },
+                },
+              ],
+            },
+          ]}
+        />
+      </div>
+      <div className="w-[330px] h-[10px] relative bottom-[58px] bg-[#f5f5f5] "></div>
     </div>
   );
 };
