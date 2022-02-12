bb2931035b4de050cb0d768a4f332010ca4edc50
Ho3ein Zarrabi <ho3einzarrabi@gmail.com>
2022-02-12 15:05:58 +0330

    fix test

--- yadesh/src/Components/AdminPanelCourses/Table.js
+++ yadesh/src/Components/AdminPanelCourses/Table.js
@@ -7,83 +7,82 @@ import { useContext } from "react";
 import { MainCounter } from "../../Context/Context";
 
 const Table = () => {
-  const [twoOfCourses, setTwoOfCourses] = useState([])
   const {
     siteManagmentDatabase,
+    setsiteManagmentDatabase,
     emtyCourse,
-    setemtyCourse
+    setemtyCourse,
   } = useContext(MainCounter);
 
   const data = siteManagmentDatabase.Courses;
   const [showmore, setShowmore] = useState(false);
+  let [clickCount, setclickCount] = useState(0);
 
-  useEffect(() => {
-    for (let i = 0; i < 2; i++) {
-      let loopResult = data[i];
-      twoOfCourses.push(loopResult);
-      const arr = [...twoOfCourses, loopResult]
-      setTwoOfCourses(arr)
-    }
-  }, []);
+  let threeOfCourses = [data[0], data[1], data[2]];
+
+  const deleteCourse = (i) => {
+    let database = JSON.parse(JSON.stringify(siteManagmentDatabase));
+    database.Courses.splice(i, 1);
+    setsiteManagmentDatabase(database);
+  };
+
   return (
     <section className="w-[97vw]">
-
-      {
-        emtyCourse
-          ?
-          <section className=" m-auto text-[12px] text-[#001D29] mr-[120px]">
-            <section className="w-full justify-between flex  py-[20px] bg-[#e6e9eb6e] rounded-lg">
-              <section className="w-[50%] pr-[10px] font-semibold">
-                {" "}
-                لیست دوره های شما
-              </section>
-              <section className="w-[600px] flex justify-between pl-[10px]">
-                <div className="flex text-[12px] w-[300px] h-[35px] bg-[#E6E9EB] rounded">
-                  <div className="mt-[13px] mx-[10px]">
-                    <Search />
-                  </div>
-                  <label className="z-30 w-[300px] h-[35px] flex">
-                    <input
-                      placeholder="جستوجوی دوره ها "
-                      className="z-30 w-[100px] placeholder:text-[#000000a4] placeholder:text-[10px] bg-[#E6E9EB] outline-none"
-                    />
-                  </label>
-                </div>
-                <div className="flex items-center justify-center text-[10px] w-[250px] h-[35px] bg-[#E6E9EB] rounded">
-                  <p> چیدمان بر اساس لیست شرکت ها</p>
-                  <div className=" mx-[10px]">
-                    <Sort />
-                  </div>
-                </div>
-              </section>
+      {emtyCourse ? (
+        <section className=" m-auto text-[12px] text-[#001D29] mr-[120px]">
+          <section className="w-full justify-between flex  py-[20px] bg-[#e6e9eb6e] rounded-lg">
+            <section className="w-[50%] pr-[10px] font-semibold">
+              {" "}
+              لیست دوره های شما
             </section>
-            <table class="table-auto w-full bg-[#e6e9eb6e]">
-              <thead>
-                <tr className="h-[50px]">
-                  <th>
-                    <p className="text-center">ردیف</p>
-                  </th>
-                  <th>
-                    <p className="text-center">تصویر دوره</p>
-                  </th>
-                  <th>
-                    <p className="text-center">نام مدرس </p>
-                  </th>
-                  <th>
-                    <p className="text-center">زمان دوره </p>
-                  </th>
-                  <th>
-                    <p className="text-center">تعداد اپیزود</p>
-                  </th>
-                  <th>
-                    <p className="text-center">دسته بندی ها</p>
-                  </th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody className="w-full">
-                {!showmore
-                  ? twoOfCourses.map((item, i) => {
+            <section className="w-[600px] flex justify-between pl-[10px]">
+              <div className="flex text-[12px] w-[300px] h-[35px] bg-[#E6E9EB] rounded">
+                <div className="mt-[13px] mx-[10px]">
+                  <Search />
+                </div>
+                <label className="z-30 w-[300px] h-[35px] flex">
+                  <input
+                    placeholder="جستوجوی دوره ها "
+                    className="z-30 w-[100px] placeholder:text-[#000000a4] placeholder:text-[10px] bg-[#E6E9EB] outline-none"
+                  />
+                </label>
+              </div>
+              <div className="flex items-center justify-center text-[10px] w-[250px] h-[35px] bg-[#E6E9EB] rounded">
+                <p> چیدمان بر اساس لیست شرکت ها</p>
+                <div className=" mx-[10px]">
+                  <Sort />
+                </div>
+              </div>
+            </section>
+          </section>
+          <table class="table-auto w-full bg-[#e6e9eb6e]">
+            <thead>
+              <tr className="h-[50px]">
+                <th>
+                  <p className="text-center">ردیف</p>
+                </th>
+                <th>
+                  <p className="text-center">تصویر دوره</p>
+                </th>
+                <th>
+                  <p className="text-center">نام مدرس </p>
+                </th>
+                <th>
+                  <p className="text-center">زمان دوره </p>
+                </th>
+                <th>
+                  <p className="text-center">تعداد اپیزود</p>
+                </th>
+                <th>
+                  <p className="text-center">دسته بندی ها</p>
+                </th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody className="w-full">
+              {!showmore
+                ? threeOfCourses.map((item, i) => {
+                    if (clickCount === 3) setShowmore(true);
                     return (
                       <tr className="hover:bg-[#e6e9ebc2]">
                         <td className="text-center">{i + 1}</td>
@@ -113,12 +112,21 @@ const Table = () => {
                           </div>
                           <p className="mr-[10px] cursor-pointer text-[#0050A8]  text-[10px]">
                             ویرایش دوره
+                          </p>
+                          <p
+                            onClick={(i) => {
+                              deleteCourse(i);
+                              setclickCount(clickCount + 1);
+                            }}
+                            className="mr-[10px] cursor-pointer text-[#0050A8]  text-[10px]"
+                          >
+                            حذف دوره
                           </p>
                         </td>
                       </tr>
                     );
                   })
-                  : data.map((item, i) => {
+                : data.map((item, i) => {
                     return (
                       <tr className="hover:bg-[#e6e9ebc2]">
                         <td className="text-center">{i + 1}</td>
@@ -149,29 +157,45 @@ const Table = () => {
                           <p className="mr-[10px] cursor-pointer text-[#0050A8]  text-[10px]">
                             ویرایش دوره
                           </p>
+                          <p
+                            onClick={(i) => deleteCourse(i)}
+                            className="mr-[10px] cursor-pointer text-[#0050A8]  text-[10px]"
+                          >
+                            حذف دوره
+                          </p>
                         </td>
                       </tr>
                     );
                   })}
-              </tbody>
-            </table>
-            <section className="w-full h-[50px] bg-[#E6E9EB] mt-[20px] flex justify-center items-center rounded-lg">
-              <p onClick={() => setShowmore(!showmore)} className="cursor-pointer ">
-                موارد بیشتر
-              </p>
-            </section>
+            </tbody>
+          </table>
+          <section className="w-full h-[50px] bg-[#E6E9EB] mt-[20px] flex justify-center items-center rounded-lg">
+            <p
+              onClick={() => setShowmore(!showmore)}
+              className="cursor-pointer "
+            >
+              موارد بیشتر
+            </p>
           </section>
-          :
-          <section className="w-full h-[400px] text-[12px] bg-[#FFF] m-auto ">
-            <section className="flex flex-col justify-center items-center w-full h-[400px] leading-6">
-              <EmtyLogo />
-              <p className="mt-[10px] text-[14px]">لیست دوره های شما خالی است !</p>
-              <p className=" text-[#0000005d] mt-[5px]"> برای افزودن دوره جدید روی دکمه زیر کلیک کنید </p>
-              <button className="w-[200px] cursor-pointer h-[35px] rounded-lg bg-[#3B8044] mt-[15px] text-[#fff]"> افزودن دوره</button>
-            </section>
+        </section>
+      ) : (
+        <section className="w-full h-[400px] text-[12px] bg-[#FFF] m-auto ">
+          <section className="flex flex-col justify-center items-center w-full h-[400px] leading-6">
+            <EmtyLogo />
+            <p className="mt-[10px] text-[14px]">
+              لیست دوره های شما خالی است !
+            </p>
+            <p className=" text-[#0000005d] mt-[5px]">
+              {" "}
+              برای افزودن دوره جدید روی دکمه زیر کلیک کنید{" "}
+            </p>
+            <button className="w-[200px] cursor-pointer h-[35px] rounded-lg bg-[#3B8044] mt-[15px] text-[#fff]">
+              {" "}
+              افزودن دوره
+            </button>
           </section>
-      }
-
+        </section>
+      )}
     </section>
   );
 };
--- yadesh/src/Components/AdminPanelSubscription/Table.js
+++ yadesh/src/Components/AdminPanelSubscription/Table.js
@@ -1,184 +1,215 @@
 import Sort from "../../assets/svg/AdminPanelCourses/Sort";
 import Search from "../../assets/svg/AdminPanelCourses/Search";
 import Successful from "../../assets/svg/AdminPanelCourses/Successful";
-import { useState, useEffect } from "react"
-import EditSubscription from "../../Components/Modals/EditSubscription"
-
+import { useState, useEffect } from "react";
+import EditSubscription from "../../Components/Modals/EditSubscription";
 
 const Table = () => {
-    const [EditSubscriptionModal, setEditSubscriptionModal] = useState(false)
-    const [twoOfSubscription, setTwoOfSubscription] = useState([])
-    let loopResult = ""
-    useEffect(() => {
-        for (let i = 0; i < 3; i++) {
-            loopResult = Subscription[i];
-             let arr = [...twoOfSubscription, loopResult]
-            setTwoOfSubscription(arr)
-        }
-    }, []
-    
-    
-    )
-    const [showmore, setShowmore] = useState(false);
-    const [Subscription, setSubscription] = useState([
-        {
-            Subscription: "1 مخابرات ایران",
-            Condition: "پرداخت شده",
-            Amount: "735 هزار تومان",
-            numberOfUsers: "132 نفر",
-            DateOfRenewal: "26 مهر 1399",
-        },
-        {
-            Subscription: " 2 مخابرات ایران",
-            Condition: "پرداخت شده",
-            Amount: "735 هزار تومان",
-            numberOfUsers: "132 نفر",
-            DateOfRenewal: "26 مهر 1399",
-        },
-        {
-            Subscription: " 3 مخابرات ایران",
-            Condition: "پرداخت شده",
-            Amount: "735 هزار تومان",
-            numberOfUsers: "132 نفر",
-            DateOfRenewal: "26 مهر 1399",
-        },
-        {
-            Subscription: " 4 مخابرات ایران",
-            Condition: "پرداخت شده",
-            Amount: "735 هزار تومان",
-            numberOfUsers: "132 نفر",
-            DateOfRenewal: "26 مهر 1399",
-        },
-        {
-            Subscription: "مخابرات ایران 5",
-            Condition: "پرداخت شده",
-            Amount: "735 هزار تومان",
-            numberOfUsers: "132 نفر",
-            DateOfRenewal: "26 مهر 1399",
-        },
-    ])
+  const [EditSubscriptionModal, setEditSubscriptionModal] = useState(false);
+  const [twoOfSubscription, setTwoOfSubscription] = useState([]);
+  let loopResult = [];
+  for (let i = 0; i < 3; i++) {
+    loopResult = Subscription[i];
+    let arr = [...twoOfSubscription, loopResult];
+    setTwoOfSubscription(arr);
+  }
 
+  const [showmore, setShowmore] = useState(false);
+  const [Subscription, setSubscription] = useState([
+    {
+      Subscription: "1 مخابرات ایران",
+      Condition: "پرداخت شده",
+      Amount: "735 هزار تومان",
+      numberOfUsers: "132 نفر",
+      DateOfRenewal: "26 مهر 1399",
+    },
+    {
+      Subscription: " 2 مخابرات ایران",
+      Condition: "پرداخت شده",
+      Amount: "735 هزار تومان",
+      numberOfUsers: "132 نفر",
+      DateOfRenewal: "26 مهر 1399",
+    },
+    {
+      Subscription: " 3 مخابرات ایران",
+      Condition: "پرداخت شده",
+      Amount: "735 هزار تومان",
+      numberOfUsers: "132 نفر",
+      DateOfRenewal: "26 مهر 1399",
+    },
+    {
+      Subscription: " 4 مخابرات ایران",
+      Condition: "پرداخت شده",
+      Amount: "735 هزار تومان",
+      numberOfUsers: "132 نفر",
+      DateOfRenewal: "26 مهر 1399",
+    },
+    {
+      Subscription: "مخابرات ایران 5",
+      Condition: "پرداخت شده",
+      Amount: "735 هزار تومان",
+      numberOfUsers: "132 نفر",
+      DateOfRenewal: "26 مهر 1399",
+    },
+  ]);
 
-    const removeItemOftwoOfSubscription = (i) => {
-        const arr = [...twoOfSubscription]
-        arr.splice(i, 1)
-        setTwoOfSubscription(arr)
-    }
+  const removeItemOftwoOfSubscription = (i) => {
+    const arr = [...twoOfSubscription];
+    arr.splice(i, 1);
+    setTwoOfSubscription(arr);
+  };
 
-    const removeItemOfSubscription = (i) => {
-        const arr = [...Subscription]
-        arr.splice(i, 1)
-        setSubscription(arr)
-    }
-    return (
-        <section className="w-[97vw]">
-            <EditSubscription EditSubscriptionModal={EditSubscriptionModal} setEditSubscriptionModal={setEditSubscriptionModal} />
+  const removeItemOfSubscription = (i) => {
+    const arr = [...Subscription];
+    arr.splice(i, 1);
+    setSubscription(arr);
+  };
+  return (
+    <section className="w-[97vw]">
+      <EditSubscription
+        EditSubscriptionModal={EditSubscriptionModal}
+        setEditSubscriptionModal={setEditSubscriptionModal}
+      />
 
-            {
-                Subscription.le
-            }
-            <section className="w-[87%] mr-[120px] m-auto text-[12px] text-[#001D29]">
-                <section className="w-full justify-between flex py-[20px]  items-center  bg-[#e6e9eb6e]">
-                    <section className="w-[50%] font-semibold pr-[10px]"> لیست اشتراک ها</section>
-                    <section className="w-[570px] flex justify-between pl-[10px]">
-                        <div className="flex text-[12px] w-[300px] h-[35px] bg-[#E6E9EB] rounded">
-                            <div className="mt-[13px] mx-[10px]">
-                                <Search />
-                            </div>
-                            <label className="z-20">
-                                <input
-                                    className=" placeholder:text-[10px] placeholder:text-[#0000008f] w-[250px] h-[35px] bg-[#E6E9EB] outline-none text-[12px]"
-                                    placeholder="جست وجو ی اشتراک"
-                                />
-                            </label>
-                        </div>
-                        <div className="flex items-center justify-center text-[10px] w-[250px] h-[35px] bg-[#E6E9EB] rounded">
-                            <p> چیدمان بر اساس لیست شرکت ها</p>
-                            <div className=" mx-[10px]">
-                                <Sort />
-                            </div>
-                        </div>
-                    </section>
-                </section>
-                <table class="table-auto w-full bg-[#e6e9eb6e] rounded text-[12px]">
-                    <thead>
-                        <tr className="text-[#000000e5] h-[50px]">
-                            <th><p className="text-center">ردیف</p></th>
-                            <th><p className="text-center">اشتراک</p></th>
-                            <th><p className="text-center">وضعیت</p></th>
-                            <th><p className="text-center">مبلغ</p></th>
-                            <th><p className="text-center">تعداد کاربران</p></th>
-                            <th><p className="text-center">تاریخ تمدید</p></th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            !showmore ?
-                                twoOfSubscription.map((item, i) => {
-                                    return (
-                                        <tr className="h-[50px] hover:bg-[#e6e9ebc2]">
-                                            <td className="text-center">{i + 1}</td>
-                                            <td className="text-center">{item.Subscription}</td>
-                                            <td className=" text-[#008043] font-semibold ">
-                                                <div className="justify-center flex ">
-                                                    <p className="ml-[5px]">{item.Condition}</p>
-                                                    <Successful />
-                                                </div>
-                                            </td>
-                                            <td className="text-center text-[#0050A8] font-semibold">{item.Amount}</td>
-                                            <td className="text-center">{item.numberOfUsers}</td>
-                                            <td className="text-center">{item.DateOfRenewal}</td>
-                                            <td >
-                                                <p
-                                                    onClick={() => setEditSubscriptionModal(!EditSubscriptionModal)}
-                                                    className="mr-[10px] text-[#0050A8] cursor-pointer text-center font-semibold">جزییات اشتراک</p>
-                                            </td>
-                                            <td >
-                                                <p
-                                                    onClick={() => removeItemOftwoOfSubscription(i)}
-                                                    className={`mr-[10px] text-[#AA0029]   cursor-pointer text-center font-semibold`}>لغو اشتراک</p>
-                                            </td>
-                                        </tr>
-                                    )
-                                })
-                                :
-                                Subscription.map((item, i) => {
-                                    return (
-                                        <tr className="h-[50px] hover:bg-[#e6e9ebc2]">
-                                            <td className="text-center">{i + 1}</td>
-                                            <td className="text-center">{item.name}</td>
-                                            <td className=" text-[#008043] font-semibold ">
-                                                <div className="justify-center flex ">
-                                                    <p className="ml-[5px]">{item.Condition}</p>
-                                                    <Successful />
-                                                </div>
-                                            </td>
-                                            <td className="text-center text-[#0050A8] font-semibold"> {item.Amount} </td>
-                                            <td className="text-center"> {item.numberOfUsers}</td>
-                                            <td className="text-center">{item.DateOfRenewal}</td>
-                                            <td >
-                                                <p
-                                                    onClick={() => setEditSubscriptionModal(!EditSubscriptionModal)}
-                                                    className="mr-[10px] text-[#0050A8]  cursor-pointer text-center font-semibold">جزییات اشتراک</p>
-                                            </td>
-                                            <td >
-                                                <p
-                                                    onClick={() => removeItemOfSubscription(i)}
-                                                    className="mr-[10px] text-[#AA0029] cursor-pointer text-center font-semibold">لغو اشتراک</p>
-                                            </td>
-                                        </tr>
-                                    )
-                                })
-                        }
-                    </tbody>
-                </table>
-                <section onClick={() => setShowmore(!showmore)} className="w-full mt-[20px] cursor-pointer h-[50px] bg-[#E6E9EB] flex justify-center rounded items-center">
-                    <p>موارد بیشتر</p>
-                </section>
-            </section>
+      {Subscription.le}
+      <section className="w-[87%] mr-[120px] m-auto text-[12px] text-[#001D29]">
+        <section className="w-full justify-between flex py-[20px]  items-center  bg-[#e6e9eb6e]">
+          <section className="w-[50%] font-semibold pr-[10px]">
+            {" "}
+            لیست اشتراک ها
+          </section>
+          <section className="w-[570px] flex justify-between pl-[10px]">
+            <div className="flex text-[12px] w-[300px] h-[35px] bg-[#E6E9EB] rounded">
+              <div className="mt-[13px] mx-[10px]">
+                <Search />
+              </div>
+              <label className="z-20">
+                <input
+                  className=" placeholder:text-[10px] placeholder:text-[#0000008f] w-[250px] h-[35px] bg-[#E6E9EB] outline-none text-[12px]"
+                  placeholder="جست وجو ی اشتراک"
+                />
+              </label>
+            </div>
+            <div className="flex items-center justify-center text-[10px] w-[250px] h-[35px] bg-[#E6E9EB] rounded">
+              <p> چیدمان بر اساس لیست شرکت ها</p>
+              <div className=" mx-[10px]">
+                <Sort />
+              </div>
+            </div>
+          </section>
         </section>
-    );
-}
-export default Table
+        <table class="table-auto w-full bg-[#e6e9eb6e] rounded text-[12px]">
+          <thead>
+            <tr className="text-[#000000e5] h-[50px]">
+              <th>
+                <p className="text-center">ردیف</p>
+              </th>
+              <th>
+                <p className="text-center">اشتراک</p>
+              </th>
+              <th>
+                <p className="text-center">وضعیت</p>
+              </th>
+              <th>
+                <p className="text-center">مبلغ</p>
+              </th>
+              <th>
+                <p className="text-center">تعداد کاربران</p>
+              </th>
+              <th>
+                <p className="text-center">تاریخ تمدید</p>
+              </th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {!showmore
+              ? twoOfSubscription.map((item, i) => {
+                  return (
+                    <tr className="h-[50px] hover:bg-[#e6e9ebc2]">
+                      <td className="text-center">{i + 1}</td>
+                      <td className="text-center">{item.Subscription}</td>
+                      <td className=" text-[#008043] font-semibold ">
+                        <div className="justify-center flex ">
+                          <p className="ml-[5px]">{item.Condition}</p>
+                          <Successful />
+                        </div>
+                      </td>
+                      <td className="text-center text-[#0050A8] font-semibold">
+                        {item.Amount}
+                      </td>
+                      <td className="text-center">{item.numberOfUsers}</td>
+                      <td className="text-center">{item.DateOfRenewal}</td>
+                      <td>
+                        <p
+                          onClick={() =>
+                            setEditSubscriptionModal(!EditSubscriptionModal)
+                          }
+                          className="mr-[10px] text-[#0050A8] cursor-pointer text-center font-semibold"
+                        >
+                          جزییات اشتراک
+                        </p>
+                      </td>
+                      <td>
+                        <p
+                          onClick={() => removeItemOftwoOfSubscription(i)}
+                          className={`mr-[10px] text-[#AA0029]   cursor-pointer text-center font-semibold`}
+                        >
+                          لغو اشتراک
+                        </p>
+                      </td>
+                    </tr>
+                  );
+                })
+              : Subscription.map((item, i) => {
+                  return (
+                    <tr className="h-[50px] hover:bg-[#e6e9ebc2]">
+                      <td className="text-center">{i + 1}</td>
+                      <td className="text-center">{item.name}</td>
+                      <td className=" text-[#008043] font-semibold ">
+                        <div className="justify-center flex ">
+                          <p className="ml-[5px]">{item.Condition}</p>
+                          <Successful />
+                        </div>
+                      </td>
+                      <td className="text-center text-[#0050A8] font-semibold">
+                        {" "}
+                        {item.Amount}{" "}
+                      </td>
+                      <td className="text-center"> {item.numberOfUsers}</td>
+                      <td className="text-center">{item.DateOfRenewal}</td>
+                      <td>
+                        <p
+                          onClick={() =>
+                            setEditSubscriptionModal(!EditSubscriptionModal)
+                          }
+                          className="mr-[10px] text-[#0050A8]  cursor-pointer text-center font-semibold"
+                        >
+                          جزییات اشتراک
+                        </p>
+                      </td>
+                      <td>
+                        <p
+                          onClick={() => removeItemOfSubscription(i)}
+                          className="mr-[10px] text-[#AA0029] cursor-pointer text-center font-semibold"
+                        >
+                          لغو اشتراک
+                        </p>
+                      </td>
+                    </tr>
+                  );
+                })}
+          </tbody>
+        </table>
+        <section
+          onClick={() => setShowmore(!showmore)}
+          className="w-full mt-[20px] cursor-pointer h-[50px] bg-[#E6E9EB] flex justify-center rounded items-center"
+        >
+          <p>موارد بیشتر</p>
+        </section>
+      </section>
+    </section>
+  );
+};
+export default Table;
