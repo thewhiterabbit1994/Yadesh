3553412b588566656641927c4a42a6823fd23941
Ho3ein Zarrabi <ho3einzarrabi@gmail.com>
2022-02-16 13:50:23 +0330

    fix test

--- yadesh/src/Components/AdminPanelCourses/MiddleSection.js
+++ yadesh/src/Components/AdminPanelCourses/MiddleSection.js
@@ -3,47 +3,52 @@ import Create from "../../assets/svg/AdminPanelCourses/Create";
 import Doreha from "../../assets/svg/AdminPanelCourses/Doreha";
 import Statistics from "../../assets/svg/AdminPanelCourses/statistics";
 import { Link } from "react-router-dom";
-import { useState } from "react"
+import { useState } from "react";
 import AddCourse from "../Modals/AddCourse";
 
 const MiddleSection = () => {
-    const [AddCourseModal, setAddCourseModal] = useState();
+  const [AddCourseModal, setAddCourseModal] = useState();
 
-    return (
-        <section className="w-[97vw] h-[200px] relative top-[100px] ">
-            <AddCourse AddCourseModal={AddCourseModal} setAddCourseModal={setAddCourseModal} />
-            <section className="w-[90%] flex text-[12px] m-auto justify-between font-semibold mr-[120px] items-center ">
-                <section className="flex w-[50%]">
-                    <div className=" ml-[10px]">
-                        <Doreha />
-                    </div>
-                    <p>دوره ها</p>
-                </section>
-                <section className="w-[400px] justify-between flex text-[10px]  text-[#001D29] ">
-                    <section className="w-[400px] rounded h-[50px] bg-[#008043] flex justify-around items-center">
-                        <section className="flex">
-                            <Create />
-                            <p className="text-[10px] mr-[10px] text-[#fff]">
-                                ایجاد دوره های جدید
-                            </p>
-                        </section>
-                        <div
-                            onClick={() => setAddCourseModal(!AddCourseModal)}
-                            className="tetx-[#001D29] cursor-pointer text-[10px] font-semibold bg-[#fff] w-[100px] h-[37px] rounded flex justify-center items-center">
-                            {" "}
-                            ایجاد کنید
-                        </div>
-                    </section>
-                    <section className="flex items-center w-[200px]">
-                        <div className="mx-[10px]">
-                            <Statistics />
-                        </div>
-                        <p>آمار بازدید دوره ها</p>
-                    </section>
-                </section>
-            </section>
+  return (
+    <section className="w-[97vw] h-[200px] relative top-[100px] ">
+      <AddCourse
+        AddCourseModal={AddCourseModal}
+        setAddCourseModal={setAddCourseModal}
+      />
+      <section className="w-[90%] flex text-[12px] m-auto justify-between font-semibold mr-[120px] items-center ">
+        <section className="flex w-[50%]">
+          <div className=" ml-[10px]">
+            <Doreha />
+          </div>
+          <p>دوره ها</p>
         </section>
-       
+        <section className="w-[400px] justify-between flex text-[10px]  text-[#001D29] ">
+          <section className="w-[400px] rounded h-[50px] bg-[#008043] flex justify-around items-center">
+            <section className="flex">
+              <Create />
+              <p className="text-[10px] mr-[10px] text-[#fff]">
+                ایجاد دوره های جدید
+              </p>
+            </section>
+            <div
+              onClick={() => setAddCourseModal(!AddCourseModal)}
+              className="tetx-[#001D29] cursor-pointer text-[10px] font-semibold bg-[#fff] w-[100px] h-[37px] rounded flex justify-center items-center"
+            >
+              {" "}
+              ایجاد کنید
+            </div>
+          </section>
+          <section className="flex items-center w-[200px]">
+            <div className="mx-[10px]">
+              <Statistics />
+            </div>
+            <Link className="flex" to="/admindashboard/adminpanel">
+              <p>آمار بازدید دوره ها</p>
+            </Link>
+          </section>
+        </section>
+      </section>
+    </section>
   );
 };
 export default MiddleSection;
--- yadesh/src/Components/AdminPanelEpisode/MiddleSection.js
+++ yadesh/src/Components/AdminPanelEpisode/MiddleSection.js
@@ -2,47 +2,53 @@ import Exele from "../../assets/svg/AdminPanelCourses/Exele";
 import Create from "../../assets/svg/AdminPanelCourses/Create";
 import Player from "../../assets/svg/adminPanelEpisode/Player";
 import Statistics from "../../assets/svg/AdminPanelCourses/statistics";
-import { useState } from "react"
-import AddCourseForEpisodeComponent from "../../Components/Modals/AddCourseForEpisodeComponent"
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import AddCourseForEpisodeComponent from "../../Components/Modals/AddCourseForEpisodeComponent";
 
 const MiddleSection = () => {
-    const [ModalEpisode, setModalEpisode] = useState(false);
+  const [ModalEpisode, setModalEpisode] = useState(false);
 
-    return (
-        <section className="w-[97vw] h-[200px] relative top-[100px] ">
-            <AddCourseForEpisodeComponent ModalEpisode={ModalEpisode} setModalEpisode={setModalEpisode} />
-            <section className=" w-[90%] mr-[130px] flex text-[12px] m-auto justify-between font-semibold items-center">
-                <section className="flex w-[50%]">
-                    <div className=" ml-[10px]">
-                        <Player />
-                    </div>
-                    <p>اپیزود ها</p>
-                </section>
-                <section className="w-[430px] justify-between flex text-[10px] text-[#001D29]">
-                    <section className="w-[270px] rounded h-[50px] bg-[#008043] flex justify-around items-center">
-                        <section className="flex">
-                            <Create />
-                            <p className="text-[10px] mr-[10px] text-[#fff]">
-                                آپلود اپیزود جدید
-                            </p>
-                        </section>
-                        <div
-                            onClick={() => setModalEpisode(!ModalEpisode)}
-                            className="tetx-[#001D29] cursor-pointer text-[10px] font-semibold bg-[#fff] w-[110px] h-[35px] rounded flex justify-center items-center">
-                            {" "}
-                            آپلود کنید
-                        </div>
-                    </section>
-                    <section className="flex items-center w-[140px]">
-                        <div className="mx-[10px]">
-                            <Statistics />
-                        </div>
-                        <p>آمار بازدید اپیزودها</p>
-                    </section>
-                </section>
-            </section>
+  return (
+    <section className="w-[97vw] h-[200px] relative top-[100px] ">
+      <AddCourseForEpisodeComponent
+        ModalEpisode={ModalEpisode}
+        setModalEpisode={setModalEpisode}
+      />
+      <section className=" w-[90%] mr-[130px] flex text-[12px] m-auto justify-between font-semibold items-center">
+        <section className="flex w-[50%]">
+          <div className=" ml-[10px]">
+            <Player />
+          </div>
+          <p>اپیزود ها</p>
         </section>
-    );
-}
-export default MiddleSection
+        <section className="w-[430px] justify-between flex text-[10px] text-[#001D29]">
+          <section className="w-[270px] rounded h-[50px] bg-[#008043] flex justify-around items-center">
+            <section className="flex">
+              <Create />
+              <p className="text-[10px] mr-[10px] text-[#fff]">
+                آپلود اپیزود جدید
+              </p>
+            </section>
+            <div
+              onClick={() => setModalEpisode(!ModalEpisode)}
+              className="tetx-[#001D29] cursor-pointer text-[10px] font-semibold bg-[#fff] w-[110px] h-[35px] rounded flex justify-center items-center"
+            >
+              {" "}
+              آپلود کنید
+            </div>
+          </section>
+          <section className="flex items-center w-[140px]">
+            <div className="mx-[10px]">
+              <Statistics />
+            </div>
+            <Link className="flex" to="/admindashboard/adminpanel">
+              <p>آمار بازدید اپیزودها</p>
+            </Link>
+          </section>
+        </section>
+      </section>
+    </section>
+  );
+};
+export default MiddleSection;
